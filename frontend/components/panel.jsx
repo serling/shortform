041b312a27acaa08bd75654d6f2119a701ed1b6b
Thomas Christoffersen <t.christoffersen@creuna.no>
2019-09-22 01:22:57 +0200

    panel logic

--- frontend/components/panel.jsx
+++ frontend/components/panel.jsx
@@ -10,15 +10,23 @@ const Panel = ({ children, isFloated }) => {
       </div>
       <style jsx>{`
         .panel {
-          box-shadow: 2px 2px 5px 0px black;
+          $break-at-sm: 25rem; //400px
+          $break-at-md: 50rem; //800px
+          $break-at-lg: 64rem; //1024px
+
+          box-shadow: 3px 3px 20px 0px #aca9a9;
           border-radius: 0.2rem;
           background-color: white;
           padding: 1.5rem;
 
           &--float {
-            float: right;
-            width: 40%;
-            margin: 0 0 0.5rem 1rem;
+            margin: 1rem 0;
+
+            @media screen and (min-width: $break-at-sm) {
+              float: right;
+              width: 40%;
+              margin: 0 0 0.5rem 1rem;
+            }
           }
         }
       `}</style>
