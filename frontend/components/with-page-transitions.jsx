9c3141db18f1d89cf7e20bfeb31f7d9a28cf7acb
Thomas Christoffersen <t.christoffersen@creuna.no>
2019-09-25 15:28:26 +0200

    themes now work in page transitions

--- frontend/components/with-page-transitions.jsx
+++ frontend/components/with-page-transitions.jsx
@@ -4,66 +4,109 @@ import { PageTransition } from "next-page-transitions";
 
 import PageLoader from "./page-loader";
 
-const TIMEOUT = 400;
+const TIMEOUT = 300;
 
 //TODO: implement animations
 const themes = {
   left: "left",
-  right: "right"
+  right: "right",
+  top: "top"
 };
 
 // https://github.com/illinois/next-page-transitions/tree/master/examples/delayed-enter
-const WithPageTransition = ({ children, theme }) => (
-  <>
-    <PageTransition
-      timeout={TIMEOUT}
-      classNames={cn("page-transition", {
-        [`page-transition--${themes[theme]}`]: themes[theme]
-      })}
-      loadingComponent={<PageLoader />}
-      loadingDelay={0}
-      loadingTimeout={{
-        enter: TIMEOUT,
-        exit: 0
-      }}
-      loadingClassNames="loading-indicator"
-    >
-      {children}
-    </PageTransition>
+const WithPageTransition = ({ children, theme }) => {
+  return (
+    <>
+      <PageTransition
+        timeout={TIMEOUT}
+        classNames={`page-transition--${themes[theme]}`}
+        loadingComponent={<PageLoader />}
+        loadingDelay={0}
+        loadingTimeout={{
+          enter: TIMEOUT,
+          exit: 0
+        }}
+        loadingClassNames="loading-indicator"
+      >
+        {children}
+      </PageTransition>
 
-    <style jsx global>{`
-      .page-transition-enter {
-        opacity: 0;
-        transform: translate3d(-20px, 0, 0);
-      }
-      .page-transition-enter-active {
-        opacity: 1;
-        transform: translate3d(0, 0, 0);
-        transition: opacity ${TIMEOUT}ms, transform ${TIMEOUT}ms;
-      }
-      .page-transition-exit {
-        opacity: 1;
-      }
-      .page-transition-exit-active {
-        opacity: 0;
-        transition: opacity ${TIMEOUT}ms;
-      }
-      .loading-indicator-appear,
-      .loading-indicator-enter {
-        opacity: 0;
-      }
-      .loading-indicator-appear-active,
-      .loading-indicator-enter-active {
-        opacity: 1;
-        transition: opacity ${TIMEOUT}ms;
-      }
-    `}</style>
-  </>
-);
+      <style jsx global>{`
+        .page-transition--top-enter {
+          opacity: 0;
+          transform: translate3d(0, -30px, 0);
+        }
+
+        .page-transition--top-enter-active {
+          opacity: 1;
+          transform: translate3d(0, 0, 0);
+          transition: opacity ${TIMEOUT}ms, transform ${TIMEOUT}ms;
+        }
+        .page-transition--top-exit {
+          opacity: 1;
+        }
+        .page-transition--top-exit-active {
+          opacity: 0;
+          transition: opacity ${TIMEOUT}ms;
+        }
+
+        .page-transition--left-enter {
+          opacity: 0;
+          transform: translate3d(-30px, 0, 0);
+        }
+
+        .page-transition--left-enter-active {
+          opacity: 1;
+          transform: translate3d(0, 0, 0);
+          transition: opacity ${TIMEOUT}ms, transform ${TIMEOUT}ms;
+        }
+        .page-transition--left-exit {
+          opacity: 1;
+        }
+        .page-transition--left-exit-active {
+          opacity: 0;
+          transition: opacity ${TIMEOUT}ms;
+        }
+
+        .page-transition--right-enter {
+          opacity: 0;
+          transform: translate3d(30px, 0, 0);
+        }
+
+        .page-transition--right-enter-active {
+          opacity: 1;
+          transform: translate3d(0, 0, 0);
+          transition: opacity ${TIMEOUT}ms, transform ${TIMEOUT}ms;
+        }
+        .page-transition--right-exit {
+          opacity: 1;
+        }
+        .page-transition--right-exit-active {
+          opacity: 0;
+          transition: opacity ${TIMEOUT}ms;
+        }
+
+        .loading-indicator-appear,
+        .loading-indicator-enter {
+          opacity: 0;
+        }
+        .loading-indicator-appear-active,
+        .loading-indicator-enter-active {
+          opacity: 1;
+          transition: opacity ${TIMEOUT}ms;
+        }
+      `}</style>
+    </>
+  );
+};
 
 WithPageTransition.propTypes = {
   children: PropTypes.node,
   theme: PropTypes.string
+};
+
+WithPageTransition.defaultProps = {
+  theme: themes.right
 };
 
 export default WithPageTransition;
